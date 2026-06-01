444376a4ad93ee2ae0e6e92212ef40ca619081e0
Rimjhim Kumari <rimjhimkumari1127@gmail.com>
2026-06-01 18:38:08 +0530

    commit

--- java scipt/JS/java/Demo.java
+++ java scipt/JS/java/Demo.java
@@ -1,5 +1,5 @@
-public class Demo{
-    public static void main(string[] args){
-        system.out.println(x:"Hello, World!");
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
     }
 }
